473eb526d976e43d65bf46bab0913b12da51f3b0
Jake Paine <jake.paine@live.com>
2013-09-06 08:30:56 +0100

    Remove error logs.

--- release/application/logs/log-2013-08-10.php
+++
@@ -1,3 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-08-10 09:38:24 --> 404 Page Not Found --> icreate
--- release/application/logs/log-2013-08-16.php
+++
@@ -1,5 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-08-16 11:13:28 --> Severity: Notice  --> Undefined index:  /Applications/MAMP/htdocs/DigitalSignage/release/application/views/news_view.php 28
-ERROR - 2013-08-16 11:13:28 --> Severity: Notice  --> Undefined index:  /Applications/MAMP/htdocs/DigitalSignage/release/application/views/news_view.php 28
-ERROR - 2013-08-16 11:13:28 --> Severity: Notice  --> Undefined index:  /Applications/MAMP/htdocs/DigitalSignage/release/application/views/news_view.php 28
