eec23f9c7282049e83b1f16c7210138a781c0ebc
Jake Paine <jake.paine@live.com>
2013-09-06 08:39:34 +0100

    Format JS a bit.

--- release/js/plugins.js
+++ release/js/plugins.js
@@ -26,39 +26,37 @@
            jQuery('.slideshow').cycle({ fx: 'fade' });
  });
 
-        var delayb4scroll=1000 //Specify initial delay before marquee starts to scroll on page (2000=2 seconds)
-        var marqueespeed=1 //Specify marquee scroll speed (larger is faster 1-10)
-        var pauseit=0 //Pause marquee onMousever (0=no. 1=yes)?
+var delayb4scroll=1000      // Specify initial delay before marquee starts to scroll on page (2000=2 seconds)
+var marqueespeed=1          // Specify marquee scroll speed (larger is faster 1-10)
+var pauseit=0               // Pause marquee onMousever (0=no. 1=yes)?
 
-        ////NO NEED TO EDIT BELOW THIS LINE////////////
+var copyspeed=marqueespeed
+var pausespeed=(pauseit==0)? copyspeed: 0
+var actualheight=''
 
-        var copyspeed=marqueespeed
-        var pausespeed=(pauseit==0)? copyspeed: 0
-        var actualheight=''
+function scrollmarquee(){
+if (parseInt(cross_marquee.style.top)>(actualheight*(-1)+8))
+    cross_marquee.style.top=parseInt(cross_marquee.style.top)-copyspeed+"px"
+else
+    cross_marquee.style.top=parseInt(marqueeheight)+8+"px"
+}
 
-        function scrollmarquee(){
-        if (parseInt(cross_marquee.style.top)>(actualheight*(-1)+8))
-        cross_marquee.style.top=parseInt(cross_marquee.style.top)-copyspeed+"px"
-        else
-        cross_marquee.style.top=parseInt(marqueeheight)+8+"px"
-        }
-
-        function initializemarquee(){
-        cross_marquee=document.getElementById("vmarquee")
-        cross_marquee.style.top=0
-        marqueeheight=document.getElementById("marqueecontainer").offsetHeight
-        actualheight=cross_marquee.offsetHeight
-        if (window.opera || navigator.userAgent.indexOf("Netscape/7")!=-1){ //if Opera or Netscape 7x, add scrollbars to scroll and exit
+function initializemarquee(){
+    cross_marquee=document.getElementById("vmarquee")
+    cross_marquee.style.top=0
+    marqueeheight=document.getElementById("marqueecontainer").offsetHeight
+    actualheight=cross_marquee.offsetHeight
+    if (window.opera || navigator.userAgent.indexOf("Netscape/7")!=-1){ //if Opera or Netscape 7x, add scrollbars to scroll and exit
         cross_marquee.style.height=marqueeheight+"px"
         cross_marquee.style.overflow="scroll"
         return
-        }
-        setTimeout('lefttime=setInterval("scrollmarquee()",30)', delayb4scroll)
-        }
+    }
+    setTimeout('lefttime=setInterval("scrollmarquee()",30)', delayb4scroll)
+}
 
-        if (window.addEventListener)
-        window.addEventListener("load", initializemarquee, false)
-        else if (window.attachEvent)
-        window.attachEvent("onload", initializemarquee)
-        else if (document.getElementById)
-        window.onload=initializemarquee
+if (window.addEventListener)
+    window.addEventListener("load", initializemarquee, false)
+else if (window.attachEvent)
+    window.attachEvent("onload", initializemarquee)
+else if (document.getElementById)
+    window.onload=initializemarquee
