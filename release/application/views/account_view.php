ae0625cc6803d0ac843405546650dd39315c1fde
Jake Paine <jake.paine@live.com>
2013-09-12 11:25:59 +0100

    Add account controller.

---
+++ release/application/views/account_view.php
@@ -0,0 +1,11 @@
+
+        <section class="wrap">
+            <hr>
+                <h1>Manage Users</h1>
+                <ol>
+                    <li>admin - admin@fairfax.bham.sch.uk - Delete</li>
+                    <li>jpaine - j.paine@fairfax.bham.sch.uk - Delete</li>
+                    <li>lsmart - l.smart@fairfax.bham.sch.uk - Delete</li>
+                </ol>
+            <hr>
+        </section>
