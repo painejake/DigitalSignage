bc39eece6d0c3c343ce39bfe3fc337f83c2bc837
Jake Paine <jake.paine@live.com>
2013-09-12 13:08:56 +0100

    Force later IE rendering.

--- release/application/views/template/dashboard/header.php
+++ release/application/views/template/dashboard/header.php
@@ -1,3 +1,4 @@
+<?php header('X-UA-Compatible: IE=edge,chrome=1'); ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
