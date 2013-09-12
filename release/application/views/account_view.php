475322abf3d3ff7ad2b8083119c8f11c9535a8b7
Jake Paine <jake.paine@live.com>
2013-09-12 13:09:32 +0100

    Some changes to account page.

--- release/application/views/account_view.php
+++ release/application/views/account_view.php
@@ -2,24 +2,7 @@
         <section class="wrap">
             <hr>
             <div class="span1">
-                <h1>Create an Account</h1>
-                <?php echo form_open('verifylogin') ?>
-                    <input type="input" autocomplete="off" id="username" name="username" placeholder="Username" /><br />
-
-                    <input type="password" autocomplete="off" id="passowrd" name="password" placeholder="Password" /><br />
-
-                    <input type="password" autocomplete="off" id="passowrd" name="password" placeholder="Confirm Password" /><br />
-
-                    <input type="input" autocomplete="off" id="email" name="email" placeholder="Email Address" /><br />
-
-                    <input type="input" autocomplete="off" id="security" name="security" placeholder="Security Code" /><br />
-
-                    <input class="button" type="submit" name="submit" value="Create Account" />
-                </form>
-            </div>
-
-            <div class="span2">
-                <h1>Change Password</h1>
+                <h1>Update Password</h1>
                 <?php echo form_open('verifylogin') ?>
                     <input type="password" autocomplete="off" id="passowrd" name="password" placeholder="Password" /><br />
 
@@ -27,14 +10,26 @@
 
                     <input class="button" type="submit" name="submit" value="Change Password" />
                 </form>
+                </form>
+            </div>
+
+            <div class="span2">
+                <h1>Update Security Code</h1>
+                <?php echo form_open('verifylogin') ?>
+                    <input type="password" autocomplete="off" id="security" name="security" placeholder="Security Code" /><br />
+
+                    <input type="password" autocomplete="off" id="securityconfirm" name="securityconfirm" placeholder="Confirm Security Code" /><br />
+
+                    <input class="button" type="submit" name="submit" value="Change Security Code" />
+                </form>
             </div>
 
             <hr>
                 <h1>Manage Users</h1>
                 <ol>
-                    <li>admin - admin@fairfax.bham.sch.uk - Delete</li>
-                    <li>jpaine - j.paine@fairfax.bham.sch.uk - Delete</li>
-                    <li>lsmart - l.smart@fairfax.bham.sch.uk - Delete</li>
+                    <li>admin - admin@fairfax.bham.sch.uk - Delete Account</li>
+                    <li>jpaine - j.paine@fairfax.bham.sch.uk - Delete Account</li>
+                    <li>lsmart - l.smart@fairfax.bham.sch.uk - Delete Account</li>
                 </ol>
             <hr>
         </section>
