b9795e8d9a2e741921a33583a25ce7aa287d24e2
painejake <jakerpaine@icloud.com>
2014-04-01 12:22:14 +0100

    Move correctly to settings_model. Todo: UPDATE as array.

--- application/controllers/settings.php
+++ application/controllers/settings.php
@@ -24,7 +24,7 @@ class Settings extends CI_Controller {
 	}
 
 	public function update() {
-		$this->load->model('settings_model'); 
+		$this->load->model('Settings_model'); 
 		$this->load->helper('form');
 		$this->load->library('form_validation');
 
@@ -40,20 +40,8 @@ class Settings extends CI_Controller {
 			$this->load->view('settings_view');
 			$this->load->view('template/dashboard/footer');
 		} else {
-			$num_news_posts			= $this->input->post('num_news_posts');
-			$num_events_posts		= $this->input->post('num_events_posts');
-			$refresh_time			= $this->input->post('refresh_time');
-			$facebook_pagename		= $this->input->post('facebook_pagename');
-			$twitter_username		= $this->input->post('twitter_username');
-			$twitter_data_id		= $this->input->post('twitter_data_id');
-
-			// this->home_model->update_settings();
-			$this->db->query("UPDATE `settings` SET `value` = '$num_news_posts' WHERE `setting` = 'num_news_posts';");
-			$this->db->query("UPDATE `settings` SET `value` = '$num_events_posts' WHERE `setting` = 'num_events_posts';");
-			$this->db->query("UPDATE `settings` SET `value` = '$refresh_time' WHERE `setting` = 'refresh_time';");
-			$this->db->query("UPDATE `settings` SET `value` = '$facebook_pagename' WHERE `setting` = 'facebook_pagename';");
-			$this->db->query("UPDATE `settings` SET `value` = '$twitter_username' WHERE `setting` = 'twitter_username';");
-			$this->db->query("UPDATE `settings` SET `value` = '$twitter_data_id' WHERE `setting` = 'twitter_data_id';");
+			
+			$this->settings_model->update_settings();
 
 			//go private area
 			redirect('settings', 'refresh');
--- application/models/settings_model.php
+++ application/models/settings_model.php
@@ -4,7 +4,7 @@ Class Settings_model extends CI_Model {
 
 	public function update_settings() {
 
-		$data = array(
+		/*$data = array(
 			'num_news_posts'		=> $this->input->post('num_news_posts'),
 			'num_events_posts'		=> $this->input->post('num_events_posts'),
 			'refresh_time'			=> $this->input->post('author'),
@@ -15,6 +15,21 @@ Class Settings_model extends CI_Model {
 			'twitter_data_id'		=> $this->input->post('twitter_data_id')
 			);
 
-		return $this->db->insert('settings', $data);
+		return $this->db->insert('settings', $data);*/
+
+		$num_news_posts			= $this->input->post('num_news_posts');
+		$num_events_posts		= $this->input->post('num_events_posts');
+		$refresh_time			= $this->input->post('refresh_time');
+		$facebook_pagename		= $this->input->post('facebook_pagename');
+		$twitter_username		= $this->input->post('twitter_username');
+		$twitter_data_id		= $this->input->post('twitter_data_id');
+
+		// this->home_model->update_settings();
+		$this->db->query("UPDATE `settings` SET `value` = '$num_news_posts' WHERE `setting` = 'num_news_posts';");
+		$this->db->query("UPDATE `settings` SET `value` = '$num_events_posts' WHERE `setting` = 'num_events_posts';");
+		$this->db->query("UPDATE `settings` SET `value` = '$refresh_time' WHERE `setting` = 'refresh_time';");
+		$this->db->query("UPDATE `settings` SET `value` = '$facebook_pagename' WHERE `setting` = 'facebook_pagename';");
+		$this->db->query("UPDATE `settings` SET `value` = '$twitter_username' WHERE `setting` = 'twitter_username';");
+		$this->db->query("UPDATE `settings` SET `value` = '$twitter_data_id' WHERE `setting` = 'twitter_data_id';");
 	}
 }
