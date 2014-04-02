957cc1fa5391658eeecdd47ba1e56a74b8b2e79e
painejake <jakerpaine@icloud.com>
2014-04-02 12:11:31 +0100

    Started adding styles for editor.

--- application/views/create_news_view.php
+++ application/views/create_news_view.php
@@ -6,6 +6,19 @@
                     <?php echo form_open('home/create_news') ?>
 
                         <p><input class="form-control" type="input" name="title" value="<?php echo set_value('title'); ?>" placeholder="News Title" /></p>
+
+                        <div class="btn-toolbar editor" role="toolbar">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-bold"></span></button>
+                                <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-italic"></span></button>
+                                <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-link"></span></button>
+                                <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-align-left"></span></button>
+                                <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-align-center"></span></button>
+                                <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-align-right"></span></button>
+                                <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-align-justify"></span></button>
+                            </div>
+                        </div>
+
                         <p><textarea class="form-control" rows="15" name="content" value="<?php echo set_value('content'); ?>" placeholder="News Content"></textarea></p>
                         <p><input class="form-control" type="input" name="author" value="<?php echo set_value('author'); ?>" placeholder="Post Author" /></p>
                         <p><input class="button" type="submit" name="submit" value="Create news entry" /></p>
